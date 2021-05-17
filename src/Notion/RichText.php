f86af4604bbfa4c6d2f798ab36d45e1d29809163
m1105 <m1105m.m1105m@gmail.com>
2021-05-17 16:25:56 +0800

    test

--- src/Notion/RichText.php
+++ src/Notion/RichText.php
@@ -2,4 +2,25 @@
 
 class RichText extends PropertyBase
 {
+    public function value(){
+        if (!is_array($this->config->rich_text)) {
+            return '';
+        }
+        return $this->config->rich_text[0]->text->content;
+    }
+    public function set($value): void{
+        if (!is_array($this->config->rich_text)){
+            $this->config->rich_text = [];
+            $this->config->rich_text[] = (object) [
+                'text' => (object) [
+                    'content' => $value,
+                ],
+            ];
+            return;
+        }
+        $this->config->rich_text[0]->text->content = $value;
+    }
+    public function getValue(){
+        return $this->config->rich_text;
+    }
 }
