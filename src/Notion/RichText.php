8b78da559f168beba85616c325a869c20f9cf940
m1105 <m1105@users.noreply.github.com>
2021-05-17 08:27:10 +0000

    Fix styling

--- src/Notion/RichText.php
+++ src/Notion/RichText.php
@@ -2,25 +2,30 @@
 
 class RichText extends PropertyBase
 {
-    public function value(){
-        if (!is_array($this->config->rich_text)) {
+    public function value()
+    {
+        if (! is_array($this->config->rich_text)) {
             return '';
         }
+
         return $this->config->rich_text[0]->text->content;
     }
-    public function set($value): void{
-        if (!is_array($this->config->rich_text)){
+    public function set($value): void
+    {
+        if (! is_array($this->config->rich_text)) {
             $this->config->rich_text = [];
             $this->config->rich_text[] = (object) [
                 'text' => (object) [
                     'content' => $value,
                 ],
             ];
+
             return;
         }
         $this->config->rich_text[0]->text->content = $value;
     }
-    public function getValue(){
+    public function getValue()
+    {
         return $this->config->rich_text;
     }
 }
